00816e9b985cbfad49db302cb895d2b64c19705c
akash0102 <akashsriram0102@gmail.com>
2022-08-20 17:07:37 +0530

    Angular promise commit

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -9,16 +9,19 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeader(id: string):Leader{
-    return LEADERS.filter((leader)=>leader.id=id)[0];
+  getLeader(id: string):Promise<Leader>{
+    return new Promise(resolve=>
+      setTimeout(()=>resolve(LEADERS.filter((leader)=>leader.id=id)[0]),2300));
   }
 
-  getLeaders(): Leader[]{
-    return LEADERS;
+  getLeaders(): Promise<Leader[]>{
+    return new Promise(resolve=>
+      setTimeout(()=>resolve(LEADERS),2300));
   }
 
-  getFeaturedLeader(): Leader {
-    return LEADERS.filter((leader)=>leader.featured)[0];
+  getFeaturedLeader(): Promise<Leader> {
+    return new Promise(resolve=>
+      setTimeout(()=>resolve(LEADERS.filter((leader)=>leader.featured)[0]),2300));
   }
   
 }
