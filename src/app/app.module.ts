8c581930464aaec798d1f2d2f291edb482f06cfa
akash0102 <akashsriram0102@gmail.com>
2022-08-20 12:02:38 +0530

    2nd week progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,11 +16,36 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LeaderService } from './services/leader.service';
+import { PromotionService } from './services/promotion.service';
+import { LoginComponent } from './login/login.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -30,9 +55,22 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FlexLayoutModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule,
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    DishService,
+    LeaderService,
+    PromotionService
+  ],
+  entryComponents: [
+        LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
